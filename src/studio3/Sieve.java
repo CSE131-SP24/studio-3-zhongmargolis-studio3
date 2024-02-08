09e3837a2a8c24fac3ec7798d40a5aa2ddaf6fe9
Yael <112411257+YaelM13@users.noreply.github.com>
2024-02-08 11:02:33 -0600

    the end

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -1,9 +1,34 @@
 package studio3;
 
+import java.util.Scanner;
+
 public class Sieve {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Choose an N: ");
+		int number = scan.nextInt();
+		
+		boolean[] listofall = new boolean[number+1];
+		
+		for (int i = 0; i < listofall.length; i++) {
+			listofall[i] = true;
+		}
+		
+		for (int factor = 2; factor < listofall.length; factor++) {
+			if (listofall[factor]) {
+				for (int p = factor*2; p <= number; p=p+factor) {
+					listofall[p] = false;
+				}
+			}
+		}
+		
+		for (int i = 0; i < listofall.length; i++) {
+			if (listofall[i]) {
+				System.out.println(i);
+			}
+		}
 
 	}
 
